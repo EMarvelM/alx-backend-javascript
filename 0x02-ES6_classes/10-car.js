00e1ec59313f0371c25ad4efd77df5b26e267c1f
EMarvelM <marvelousegbe19@gmail.com>
2024-06-30 15:56:10 +0100

    optimised code

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,18 +1,11 @@
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string') throw new TypeError('brand must be a string');
     this._brand = brand;
-    if (typeof motor !== 'string') throw new TypeError('motor must be a string');
     this._motor = motor;
-    if (typeof color !== 'string') throw new TypeError('color must be a string');
     this._color = color;
   }
 
-  static static_cloneCar(brand, motor, color) {
-    return new this(brand, motor, color);
-  }
-
   cloneCar() {
-    return this.constructor.static_cloneCar(this._brand, this._motor, this._color);
+    return new this.constructor();
   }
 }
