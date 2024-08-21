08ca2ba49d15477382b2307385d90761f938db43
EMarvelM <marvelousegbe19@gmail.com>
2024-08-21 21:45:51 +0100

    throw a new error when neccessary

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,32 +1,34 @@
 const fs = require('fs');
 
 module.exports = function countStudents(path) {
-  if (fs.accessSync(path) === undefined) {
-    const content = fs.readFileSync(path, { encoding: 'utf-8' }).split('\n').slice(1);
-
-    const obj = {};
-
-    content.forEach((da) => {
-      const lastElement = da.split(',').slice(-1)[0];
-      const firstElement = da.split(',').slice(0)[0];
-
-      if (lastElement.length < 1) {
-        return;
-      }
-      if (lastElement in obj) {
-        obj[lastElement].count += 1;
-        obj[lastElement].name.push(firstElement);
-      } else {
-        obj[lastElement] = {
-          count: 1,
-          name: [firstElement],
-        };
-      }
-    });
-    Object.keys(obj).forEach((key) => {
-      console.log(`Number of students in ${key}: ${obj[key].count}. List: ${obj[key].name.join(', ')}`);
-    });
-  } else {
-    console.log('Cannot load the database');
+  try {
+    if (fs.accessSync(path) === undefined) {
+      const content = fs.readFileSync(path, { encoding: 'utf-8' }).split('\n').slice(1);
+  
+      const obj = {};
+  
+      content.forEach((da) => {
+        const lastElement = da.split(',').slice(-1)[0];
+        const firstElement = da.split(',').slice(0)[0];
+  
+        if (lastElement.length < 1) {
+          return;
+        }
+        if (lastElement in obj) {
+          obj[lastElement].count += 1;
+          obj[lastElement].name.push(firstElement);
+        } else {
+          obj[lastElement] = {
+            count: 1,
+            name: [firstElement],
+          };
+        }
+      });
+      Object.keys(obj).forEach((key) => {
+        console.log(`Number of students in ${key}: ${obj[key].count}. List: ${obj[key].name.join(', ')}`);
+      });
+    }
+  } catch {
+    throw new Error('Cannot load the database');
   }
 };
