c7f8d3a26bca8594fd06fe4050166b0bf2135c1e
EMarvelM <marvelousegbe19@gmail.com>
2024-08-21 16:02:02 +0100

    no lint err

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
-process.stdin.resume()
+console.log('Welcome to Holberton School, what is your name?');
 
-console.log('Welcome to Holberton School, what is your name?')
+process.stdin.resume();
 process.stdin.on('data', (data) => {
-  process.stdin.pause()
-  console.log('Your name is:', data.toString().trim())
-})
+  process.stdin.pause();
+  console.log('Your name is:', data.toString().trim());
+});
 
 process.on('exit', () => {
-  console.log('This important software is now closing')
-})
+  console.log('This important software is now closing');
+});
