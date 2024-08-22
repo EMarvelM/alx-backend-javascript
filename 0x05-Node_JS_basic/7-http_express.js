280580a2ca8d0eb920a4c39db7914567e7cdb86c
EMarvelM <marvelousegbe19@gmail.com>
2024-08-22 06:16:27 +0100

    In a file named 7-http_express.js, recreate the small HTTP server using Express:
        It should be assigned to the variable app and this one must be exported
        HTTP server should listen on port 1245
        It should return plain text
        When the URL path is /, it should display Hello Holberton School! in the page body
        When the URL path is /students, it should display This is the list of our students followed by the same content as the file 3-read_file_async.js (with and without the database) - the name of the database must be passed as argument of the file
        CSV file can contain empty lines (at the end) - and they are not a valid student!

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const fs = require('fs');
+const argv = require('process');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.end('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  try {
+    let re = 'This is the list of our students\n';
+    fs.readFile(argv[2], { encoding: 'utf-8' }, (err, data) => {
+      if (err) {
+        res.statusCode = 404;
+        res.end(`${re} Cannot load the database`);
+        return;
+      }
+      const content = data.split('\n').slice(1).filter((d) => d.trim().length > 0);
+      const obj = {};
+
+      content.forEach((da) => {
+        const lastElement = da.split(',').slice(-1)[0];
+        const firstElement = da.split(',').slice(0)[0];
+
+        if (lastElement.length < 1) {
+          return;
+        }
+        if (lastElement in obj) {
+          obj[lastElement].count += 1;
+          obj[lastElement].name.push(firstElement);
+        } else {
+          obj[lastElement] = {
+            count: 1,
+            name: [firstElement],
+          };
+        }
+      });
+      re += `Number of students: ${content.length}\n`;
+      Object.keys(obj).forEach((key) => {
+        re += `Number of students in ${key}: ${obj[key].count}. List: ${obj[key].name.join(', ')}\n`;
+      });
+      re = re.slice(0, re.length - 1);
+      res.end(re);
+    });
+  } catch (err) {
+    res.statusCode = 500;
+    res.end('Cannot load the database');
+  }
+});
+
+app.listen(port);
+
+module.exports = app;
