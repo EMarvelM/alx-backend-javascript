e5aa2f7b0c63e3dd036e28259ffa9952b9065ec7
EMarvelM <marvelousegbe19@gmail.com>
2024-08-22 05:55:47 +0100

    trying a different response

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -11,8 +11,8 @@ const app = http.createServer((req, res) => {
       let re = 'This is the list of our students\n';
       fs.readFile(argv[2], { encoding: 'utf-8' }, (err, data) => {
         if (err) {
-          res.statusCode = 403;
-          res.end('Cannot load the database');
+          res.statusCode = 404;
+          res.end(re + 'Cannot load the database');
           return;
         }
         const content = data.split('\n').slice(1).filter((d) => d.trim().length > 0);
@@ -43,7 +43,7 @@ const app = http.createServer((req, res) => {
         res.end(re);
       });
     } catch (err) {
-      res.statusCode = 403;
+      res.statusCode = 500;
       res.end('Cannot load the database');
     }
   }
